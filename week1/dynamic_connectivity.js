12a9128c36f94b0f1cba0179513f16be1b617409
elvish7 <elvish7@gmail.com>
2021-07-29 15:51:36 +0800

    dc code

---
+++ week1/dynamic_connectivity.js
@@ -0,0 +1,20 @@
+/**
+Dynamic connectivity
+The algorithm of how to solve dynamic connectivity problems
+Methods of union and connect (to actually run the code) is the next topic
+ */
+
+/*
+public static void main(String[] args){
+	int N = StdIn.readInt();
+	UF uf = new UF(N);
+	while (!StdIn.isEmpty()){
+		int p = StdIn.readInt();
+		int q = StdIn.readInt();
+		if (!uf.connected(p, q)){
+			uf.union(p, q);
+			StdOut.println(p + " " + q);
+		}
+	}
+}
+*/
